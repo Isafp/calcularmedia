6c4be300a59176985968f800dc7c0c6bc0297e54
Isabela Ferreira Paranaíba <isafparanaiba@gmail.com>
2022-11-02 10:25:57 -0300

    Create codigos.py

---
+++ codigos.py
@@ -0,0 +1,14 @@
+
+#coletando dados
+q = int(input("Quantos números serão informados?" ))
+n1 = int(input("Número 1:" ))
+n2 = int(input("Número 2:" ))
+n3 = int(input("Número 3:" ))
+n4 = int(input("Número 4:" ))
+n5 = int(input("Número 5:" ))
+
+#calculando a média
+media = (n1 + n2 + n3 + n4 + n5)/(q)
+
+#exibindo o resultado
+print("Média= " + str(media))
