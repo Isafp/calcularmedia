dbe7506b606786eb1b2404c25225782b2f9a7d57
Isabela Ferreira Paranaíba <isafparanaiba@gmail.com>
2022-11-02 11:50:22 -0300

    Update codigos.py

--- codigos.py
+++ codigos.py
@@ -1,6 +1,8 @@
+import streamlit as st
 
+st.title("Simple Calculator")
 #coletando dados
-q = int(input("Quantos números serão informados?" ))
+
 n1 = int(input("Número 1:" ))
 n2 = int(input("Número 2:" ))
 n3 = int(input("Número 3:" ))
@@ -8,7 +10,7 @@ n4 = int(input("Número 4:" ))
 n5 = int(input("Número 5:" ))
 
 #calculando a média
-media = (n1 + n2 + n3 + n4 + n5)/(q)
+media = (n1 + n2 + n3 + n4 + n5)/(5)
 
 #exibindo o resultado
 print("Média= " + str(media))
